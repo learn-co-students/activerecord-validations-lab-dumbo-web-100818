e4cb24f7504893122d79a6f97dd491216cada1ba
github username <github email address>
2018-11-06 10:00:55 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  validate :validate_in?
 end
+
+
+
+  def validate_in?
+    unless title.in? ["Won't Believe", "Secret", "Top", "Guess"]
+      errors[:title] << 'Need a valid title please!'
+    end
+  end
