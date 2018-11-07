6bc28e84ea2e5591fa0a1d033a6947dbc96283cc
github username <github email address>
2018-11-06 20:27:25 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,19 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates_inclusion_of :category, :in => ["Fiction", "Non-Fiction"], :allow_nil => false
+  validate :hello_there
 
-  # def validate(post)
-  #   if post.title.include?("Guess")
-  #     true
-  #   elsif post.title.include?("Won't Believe")
-  #     true
-  #   elsif post.title.include?("Secret")
-  #     true
-  #   elsif post.title.include?("Top")
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
-
+  def hello_there
+    valid = ["Guess", "Won't Believe", "Secret", "Top"].map {|text| self.title.to_s.include?(text)}
+    if valid.uniq.length > 1
+      true
+      errors.add(:title, "must be clickbait")
+    
 end
