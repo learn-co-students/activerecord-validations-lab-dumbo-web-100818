f53be1bd10c3e70a33a27ce6edafe41f3820fd6f
achadha2 <andy.chadha2@gmail.com>
2018-11-07 09:15:02 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,18 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }
 end
+
+
+# 
+# CLICKBAIT_PATTERNS = [/Won't Believe/i, /Secret/i, /Top[0-9]*/i, /Guess/i
+# ]
+#
+# def is_clickbait?
+#   if CLICKBAIT_PATTERNS.none? {|pat| pat.match title}
+#     errors.add(:title, "must be clickbait")
+#
+#   end
+#
+# end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,33 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates_inclusion_of :category, :in => ["Fiction", "Non-Fiction"], :allow_nil => false
+  validate :hello_there
+
+  def hello_there
+    valid = ["Guess", "Won't Believe", "Secret", "Top"].map {|title| self.title.to_s.include?(title)}
+    if valid.uniq.length > 1
+      true
+    else
+      errors.add(:title, "must be clickbait")
+    end
+    # if self.title.include?("Guess")
+    #
+    # elsif self.title.include?("Won't Believe")
+    #
+    # elsif self.title.include?("Secret")
+    #
+    # elsif self.title.include?("Top")
+    #
+    # else
+    #
+    # end
+  end
+
+
+  #validates :title, inclusion: "Won't Believe" || "Secret" || "Top" || "Guess"
+
+
 end
